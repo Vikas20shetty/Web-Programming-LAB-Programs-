409bfa352bcb9b119be0678e91cdeff6969b87a1
Vikas M <vikas20012005@gmail.com>
2025-11-21 20:07:11 -0800

    Fix indentation and clean up session timeout logic

--- 2.session.php
+++ 2.session.php
@@ -3,18 +3,15 @@
 session_save_path('c:/xampp/htdocs/session'); 
 session_start(); 
  
-
 $sessionTimeout = 30; 
  
-
 if (isset($_SESSION['LAST_ACTIVITY'])) 
   { 
     $lastActivity = $_SESSION['LAST_ACTIVITY']; 
     $currentTime = time(); 
     $timeSinceLastActivity = $currentTime - $lastActivity; 
  
-   
-    if ($timeSinceLastActivity > $sessionTimeout) 
+   if ($timeSinceLastActivity > $sessionTimeout) 
       { 
        
         session_unset(); 
