01cfc357a9b85069c96636f098df67e826a695b0
Vikas M <vikas20012005@gmail.com>
2025-11-21 20:06:27 -0800

    Refactor session timeout logic for clarity

--- 2.session.php
+++ 2.session.php
@@ -1,32 +1,35 @@
 <?php 
  
-
-after 30 minutes of inactivity.*/ 
- 
 session_save_path('c:/xampp/htdocs/session'); 
 session_start(); 
  
 
-$sessionTimeout = 30; // 30 minutes 
+$sessionTimeout = 30; 
  
 
-if (isset($_SESSION['LAST_ACTIVITY'])) { 
+if (isset($_SESSION['LAST_ACTIVITY'])) 
+  { 
     $lastActivity = $_SESSION['LAST_ACTIVITY']; 
     $currentTime = time(); 
     $timeSinceLastActivity = $currentTime - $lastActivity; 
  
    
-    if ($timeSinceLastActivity > $sessionTimeout) { 
+    if ($timeSinceLastActivity > $sessionTimeout) 
+      { 
        
         session_unset(); 
         session_destroy(); 
         echo "Session expired. Please log in again."; 
-    } else { 
+      } 
+    else 
+      { 
       
         $_SESSION['LAST_ACTIVITY'] = $currentTime; 
         echo "Session active."; 
-    } 
-} else { 
+      } 
+ } 
+else 
+{ 
  
     $_SESSION['LAST_ACTIVITY'] = time(); 
     echo "Session started."; 
