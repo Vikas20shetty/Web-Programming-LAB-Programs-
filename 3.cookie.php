3fbfe567d34de791a84eb29cacb958fe69b50723
Vikas M <vikas20012005@gmail.com>
2025-11-23 06:37:19 -0800

    Update 3.cookie.php

--- 3.cookie.php
+++ 3.cookie.php
@@ -1,19 +1,22 @@
-<!DOCTYPE html> 
-<?php 
- 
-$cookie_name = "bca_5"; 
-$cookie_value = "bca_student"; 
-setcookie($cookie_name, $cookie_value, time() + 1800, "/"); 
- 
-if(!isset($_COOKIE[$cookie_name]))
-{ 
-    echo "Cookie named '" . $cookie_name . "' is not set!"; 
-} 
+<!DOCTYPE html>
+<?php
 
-else 
- 
-{ 
-    echo "Cookie '" . $cookie_name . "' is set!<br>"; 
-    echo "Value is: " . $_COOKIE[$cookie_name]; 
-} 
+if ($_SERVER["REQUEST_METHOD"] == "POST")
+
+$cookie_name =$_POST['name'];
+$cookie_value = $_POST['phone'];
+
+
+setcookie($cookie_name, $cookie_value, time() + 1800, "/"); 
+
+if(!isset($_COOKIE[$cookie_name]))
+{
+    echo"Cookie named " .$cookie_name. " is not set";
+}
+
+else
+{
+    echo"Cookie  " .$cookie_name." is set<br>";
+    echo"Value is ".$_COOKIE[$cookie_name];
+}
 ?>
